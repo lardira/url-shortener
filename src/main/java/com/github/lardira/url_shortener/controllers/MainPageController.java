e8feb8455d12bb63d278b3856e668600db98b3dc
lardira <danilkefimov@gmail.com>
2023-04-21 15:08:25 +0300

    clean up

--- src/main/java/com/github/lardira/url_shortener/controllers/MainPageController.java
+++ src/main/java/com/github/lardira/url_shortener/controllers/MainPageController.java
@@ -2,6 +2,7 @@ package com.github.lardira.url_shortener.controllers;
 
 import com.github.lardira.url_shortener.models.URL;
 import com.github.lardira.url_shortener.services.UrlService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- src/main/java/com/github/lardira/url_shortener/repositories/UrlRepository.java
+++ src/main/java/com/github/lardira/url_shortener/repositories/UrlRepository.java
@@ -6,9 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UrlRepository extends JpaRepository<URL, Integer> {
-//    URL save(URL url);
-//    URL getBy(String encodedUrlId);
-//    URL find(String url);
     public Optional<URL> findByEncodedUrlId(String encodedUrlId);
     public Optional<URL> findByOriginal(String original);
 }
